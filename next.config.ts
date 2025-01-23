69b0db3a9a739cd1f23de46304884e54232d2ead
mattmingit <131147762+mattmingit@users.noreply.github.com>
2025-01-23 12:04:45 +0100

    impl config. added initial packages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
+import createMDX from "@next/mdx";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  /* configure `pageExtensions` to include markdown and MDX files */
+  pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
 };
 
-export default nextConfig;
+const withMDX = createMDX({});
+export default withMDX(nextConfig);
